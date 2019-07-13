d94647d147d4a4e899650183a1e961312c7ab9f6
Abdul Muhaymin Arif <mampary@hotmail.com>
2019-07-13 20:50:02 +0530

    Add project files.

---
+++ Test3/CrossTest3.shared.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Plugin.Test3
+{
+    /// <summary>
+    /// Cross Test3
+    /// </summary>
+    public static class CrossTest3
+    {
+        static Lazy<ITest3> implementation = new Lazy<ITest3>(() => CreateTest3(), System.Threading.LazyThreadSafetyMode.PublicationOnly);
+
+        /// <summary>
+        /// Gets if the plugin is supported on the current platform.
+        /// </summary>
+        public static bool IsSupported => implementation.Value == null ? false : true;
+
+        /// <summary>
+        /// Current plugin implementation to use
+        /// </summary>
+        public static ITest3 Current
+        {
+            get
+            {
+                ITest3 ret = implementation.Value;
+                if (ret == null)
+                {
+                    throw NotImplementedInReferenceAssembly();
+                }
+                return ret;
+            }
+        }
+
+        static ITest3 CreateTest3()
+        {
+#if NETSTANDARD1_0 || NETSTANDARD2_0
+            return null;
+#else
+#pragma warning disable IDE0022 // Use expression body for methods
+            return new Test3Implementation();
+#pragma warning restore IDE0022 // Use expression body for methods
+#endif
+        }
+
+        internal static Exception NotImplementedInReferenceAssembly() =>
+            new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
+
+    }
+}
---
+++ Test3/Test3.android.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.Test3
+{
+    /// <summary>
+    /// Interface for Test3
+    /// </summary>
+    public class Test3Implementation : ITest3
+    {
+    }
+}
