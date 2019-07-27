4d49f18bdf974c6b737bbfac360220fe46011b8b
Abdul Muhaymin Arif <mampary@hotmail.com>
2019-07-28 00:38:33 +0530

    Not working....

---
+++ Test3/Nonsense.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.Test3
+{
+    static public class Nonsense
+    {
+        public string TestNonsense { get; set; }
+    }
+}
---
+++ Test3/OtherNonsense.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.Test3
+{
+    public class OtherNonsense
+    {
+        public Nonsense test { get; set; }
+        public OtherNonsense()
+        {
+            test = new Nonsense();
+        }
+    }
+}
